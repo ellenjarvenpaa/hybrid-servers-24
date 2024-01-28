db22f497588c8b13d3ee18e6b3174ea5f0a4e1b8
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2024-01-28 14:05:26 +0200

    viikko 2

--- hybrid-graphql-starter-main/src/api/resolvers/mediaResolver.ts
+++ hybrid-graphql-starter-main/src/api/resolvers/mediaResolver.ts
@@ -5,6 +5,7 @@ import {
   fetchMediaByTag,
   postMedia,
   postTagToMedia,
+  putMedia,
 } from '../models/mediaModel';
 import {MyContext} from '../../local-types';
 import {GraphQLError} from 'graphql';
@@ -53,6 +54,15 @@ export default {
         args.input.tag_name,
         Number(args.input.media_id),
       );
+    },
+    updateMediaItem: async (
+      _parent: undefined,
+      args: {
+        input: Pick<MediaItem, 'title' | 'description'>;
+        media_id: string;
+      },
+    ) => {
+      return await putMedia(args.input, Number(args.media_id));
     },
   },
 };
